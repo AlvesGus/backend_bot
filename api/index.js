29be2165acf8e1bdf70f012f165f9f35c2dfed97
AlvesGus <gustavo.tbt@hotmail.com.br>
2025-12-04 14:02:16 -0300

    feat: new route balance and balance of period

--- api/index.js
+++ api/index.js
@@ -28,42 +28,131 @@ app.get("/api/transactions", async (req, res) => {
     res.status(200).send(transactions);
   } catch (error) {
     console.log(error);
-    res.status(500).send({error: "Failed to fetch transactions"});
+    res.status(500).send({ error: "Failed to fetch transactions" });
   }
 });
 
 app.post("/api/add-transaction", async (req, res) => {
-  const { title, amount, type, category, telegram_id, name_user} = req.body;
+  const { title, amount, type, category, telegram_id, name_user } = req.body;
   try {
     const data = await prisma.transaction.create({
-      data: { title, amount, type, category, telegram_id, name_user}
-    })
+      data: { title, amount, type, category, telegram_id, name_user },
+    });
 
     res.status(201).send({ message: "Transaction added successfully", data });
-    
   } catch (error) {
     console.log(error);
     res.status(500).send({ error: "Failed to add transaction" });
   }
+});
 
-})
+app.get("/api/balance", async (req, res) => {
+  try {
+    const revenue = await prisma.transaction.aggregate({
+      _sum: {
+        amount: true,
+      },
+      where: {
+        category: "Entrada",
+      },
+    });
+
+    const expense = await prisma.transaction.aggregate({
+      _sum: {
+        amount: true,
+      },
+      where: {
+        category: "Saida",
+      },
+    });
+
+    const revenues = revenue._sum.amount || 0;
+    const expenses = expense._sum.amount || 0;
+
+    const balance = revenues - expenses;
+
+    res.status(200).send({
+      entradas: revenues,
+      saídas: expenses,
+      saldo: balance,
+    });
+
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({ message: "Erro! Impossível calcular" });
+  }
+});
+
+app.get("/api/balance/period", async (req, res) => {
+  try {
+    const { start, end } = req.query;
+
+    if (!start || !end) {
+      return res.status(400).send({
+        message: "Você deve enviar uma data de início e fim.",
+      });
+    }
+
+    const startDate = new Date(start);
+    const endDate = new Date(end);
+
+    const entradas = await prisma.transaction.aggregate({
+      _sum: {
+        amount: true,
+      },
+      where: {
+        category: "Entrada",
+        createdAt: {
+          gte: startDate,
+          lte: endDate,
+        },
+      },
+    });
+
+    const saidas = await prisma.transaction.aggregate({
+      _sum: {
+        amount: true,
+      },
+      where: {
+        category: "Saida",
+        createdAt: {
+          gte: startDate,
+          lte: endDate,
+        },
+      },
+    });
+
+    const totalEntradas = entradas._sum.amount || 0;
+    const totalSaidas = saidas._sum.amount || 0;
+    const saldo = totalEntradas - totalSaidas;
+
+    res.status(200).json({
+      start: startDate,
+      end: endDate,
+      entradas: totalEntradas,
+      saidas: totalSaidas,
+      saldo,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({ message: "Erro! Impossível calcular" });
+  }
+});
 
 app.delete("/api/delete-transaction/:id", async (req, res) => {
-  const {id} = req.params
+  const { id } = req.params;
 
   try {
     await prisma.transaction.delete({
-      where: { id: String(id) }
-    })
+      where: { id: String(id) },
+    });
 
-    res.status(200).send({ message: "Transaction deleted successfully"});
-
+    res.status(200).send({ message: "Transaction deleted successfully" });
   } catch (error) {
-    console.log(error)
+    console.log(error);
     res.status(500).send({ error: "Failed to delete transaction" });
   }
-
-})
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
